5f4b017bed94ec684d2a43414d96ffb7bd249b6d
justamitsaha <justamitsaha@gmail.com>
2023-02-02 12:22:58 +0530

    CORS config in API gateway

---
+++ apiGateWay/src/main/java/com/saha/amit/constants/ApiGatewayCosntants.java
@@ -0,0 +1,5 @@
+package com.saha.amit.constants;
+
+public class ApiGatewayCosntants {
+    public static final String JWT_HEADER = "Authorization";
+}
--- onboardUserService/src/main/java/com/saha/amit/OnboardUserServiceApplication.java
+++ onboardUserService/src/main/java/com/saha/amit/OnboardUserServiceApplication.java
@@ -2,10 +2,10 @@ package com.saha.amit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class OnboardUserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(OnboardUserServiceApplication.class,args);
