2ac382732f43dbb259914de45619f1cdf5171576
justamitsaha <justamitsaha@gmail.com>
2023-03-14 18:37:55 +0530

    movingaws to discovery

--- aws-service/src/main/java/com/saha/amit/AwsServiceApplication.java
+++ aws-service/src/main/java/com/saha/amit/AwsServiceApplication.java
@@ -2,8 +2,10 @@ package com.saha.amit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AwsServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AwsServiceApplication.class,args);
