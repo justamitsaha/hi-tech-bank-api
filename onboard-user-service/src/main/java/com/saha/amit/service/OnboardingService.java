65195e7ed8a535891c7d0f4146443e69310e5054
justamitsaha <justamitsaha@gmail.com>
2023-03-16 13:57:05 +0530

    Moving API call to new method

--- onboard-user-service/src/main/java/com/saha/amit/service/OnboardingService.java
+++ onboard-user-service/src/main/java/com/saha/amit/service/OnboardingService.java
@@ -8,7 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
@@ -22,15 +21,13 @@ import java.util.Date;
 
 @Service
 public class OnboardingService {
+    private static final Logger log = LoggerFactory.getLogger(OnboardingService.class);
     @Autowired
     OnboardingRepositiry onboardingRepositiry;
     @Autowired
     WebClient.Builder webClientBuilder;
-
     @Autowired
     OnBoardingProperties onBoardingProperties;
-
-    private static final Logger log= LoggerFactory.getLogger(OnboardingService.class);
 
     public String applyToOpenAccount(OnboardUserDTO onboardUser, MultipartFile multipartFile1, MultipartFile multipartFile2) throws IOException {
         log.info("Start applyToOpenAccount service");
@@ -39,27 +36,7 @@ public class OnboardingService {
         onboardUser.setEmailOtp(OnboardingUtil.generateRandomSixDigit());
         onboardUser.setTextOtp(OnboardingUtil.generateRandomSixDigit());
         onboardUser.setAttachment1Name(OnboardingUtil.getFileNameForStoring(multipartFile1, applicationId));
-        onboardUser.setAttachment2Name(OnboardingUtil.getFileNameForStoring(multipartFile2,applicationId));
-        Object awsResponse= null;
-        Object redisResponse= null;
-        Date date = new Date();
-        log.info("Redis cache API call start"+ date);
-        try {
-            redisResponse = webClientBuilder.build().post()
-                    .uri(onBoardingProperties.getRedisSaveApplicationEndPoint())
-                    .body(Mono.just(onboardUser), OnboardUserDTO.class)
-                    .retrieve()
-                    .bodyToMono(Object.class)
-                    .block();
-            log.info("Redis Response "+redisResponse.toString());
-        } catch (Exception e){
-            log.error("Error in getting Redis cache service"+ e);
-        }
-
-        Date date1 = new Date();
-        log.info("Time taken for 1st API call --> "+ (date1.getTime()- date.getTime())/1000);
-
-        log.info("AWS API call start"+ date1);
+        onboardUser.setAttachment2Name(OnboardingUtil.getFileNameForStoring(multipartFile2, applicationId));
 
         MultiValueMap<String, Object> formData = new LinkedMultiValueMap<>();
         formData.add("multipartFile", multipartFile1);
@@ -70,32 +47,55 @@ public class OnboardingService {
                 .header("Content-Disposition", "form-data; name=multipartFile; filename=profile-image.jpg");
         builder.part("fileName", onboardUser.getAttachment1Name(), MediaType.TEXT_PLAIN)
                 .header("Content-Disposition", "form-data; name=fileName").header("Content-type", "text/plain");
+        var fileUploadPayload =builder.build();
 
-        try {
-            awsResponse =webClientBuilder.build().post()
-                    .uri(onBoardingProperties.getAwsS3UploadEndpoint())
-                    .contentType(MediaType.MULTIPART_FORM_DATA)
-                    .bodyValue(builder.build())
-                    .retrieve()
-                    .bodyToMono(Object.class)
-                    .block();
-            log.info(awsResponse.toString());
-        } catch (Exception e){
-            log.error("Error in getting Redis cache service"+ e);
-        }
-        Date date2 = new Date();
-        log.info("AWS API call end "+date2);
-        log.info("AWS API call time taken "+ (date2.getTime()- date1.getTime())/1000);
-        log.info("Total time taken "+(date2.getTime()- date1.getTime())/1000);
+        makeApiCall(onBoardingProperties.getRedisSaveApplicationEndPoint(), false, OnboardUserDTO.class, onboardUser);
+        makeApiCall(onBoardingProperties.getAwsS3UploadEndpoint(), true, Object.class, fileUploadPayload);
+
 
         try {
             //onboardingRepositiry.save(onboardUser);
-        }catch (Exception e ){
-            log.error("Error adding customer" +e.toString());
+        } catch (Exception e) {
+            log.error("Error adding customer" + e.toString());
             throw e;
         }
         log.info("End applyToOpenAccount service");
+
+
+
         return onboardUser.getApplicationId();
+    }
+
+    public Object makeApiCall(String url, Boolean isFormPost, Class<?> requestClass, Object payload) {
+        Date startDate = new Date();
+        log.info("Api call for --> " + url + " started at " + startDate);
+        Object response = null;
+        if (isFormPost) {
+            try {
+                response = webClientBuilder.build().post()
+                        .uri(url)
+                        .contentType(MediaType.MULTIPART_FORM_DATA)
+                        .bodyValue(payload)
+                        .retrieve()
+                        .bodyToMono(Object.class)
+                        .block();
+                log.info(response.toString());
+            } catch (Exception e) {
+                log.error("Error in getting Redis cache service" + e);
+            }
+        } else {
+            response = webClientBuilder.build().post()
+                    .uri(url)
+                    .body(Mono.just(payload), requestClass)
+                    .retrieve()
+                    .bodyToMono(Object.class)
+                    .block();
+            log.info("For API  " + response.toString());
+        }
+        Date endDate = new Date();
+        log.info("Api call for --> " + url + " ended at " + endDate);
+        log.info("Time taken for Api call " + (endDate.getTime() - startDate.getTime()) / 1000);
+        return response;
     }
 
 }
