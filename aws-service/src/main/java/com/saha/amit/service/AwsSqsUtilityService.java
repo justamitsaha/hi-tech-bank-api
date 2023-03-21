24ca6d1a6d69da915d777c978c63f3aa2674627d
justamitsaha <justamitsaha@gmail.com>
2023-03-21 07:34:58 +0530

    Returning Onboarding DTO after reading SQS and null check

--- aws-service/src/main/java/com/saha/amit/service/AwsSqsUtilityService.java
+++ aws-service/src/main/java/com/saha/amit/service/AwsSqsUtilityService.java
@@ -1,8 +1,10 @@
 package com.saha.amit.service;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.saha.amit.config.AwsConnectionConfig;
+import com.saha.amit.dto.OnboardUserDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,16 +12,18 @@ import org.springframework.stereotype.Service;
 import software.amazon.awssdk.services.sqs.SqsClient;
 import software.amazon.awssdk.services.sqs.model.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class AwsSqsUtilityService {
 
-    private static final Logger logger = LoggerFactory.getLogger(AwsSqsUtilityService.class);
+    private static final Logger log = LoggerFactory.getLogger(AwsSqsUtilityService.class);
     @Autowired
     AwsConnectionConfig awsConnectionConfig;
 
-    public void readMessageFromQueue() {
+    public List<OnboardUserDTO> readMessageFromQueue() {
+        List<OnboardUserDTO> onboardUserDTOList = new ArrayList<>();
         try {
             ReceiveMessageRequest receiveMessageRequest = ReceiveMessageRequest.builder()
                     .queueUrl("https://sqs.ap-south-1.amazonaws.com/615839970612/on-boarding-queue")
@@ -30,17 +34,30 @@ public class AwsSqsUtilityService {
             if (messageList.size() > 0) {
                 messageList.forEach(message ->
                         {
-                            logger.info(message.body());
+                            log.info(message.body());
                             String json = message.body();
                             JsonObject convertedObject = new Gson().fromJson(json, JsonObject.class);
-                            logger.info("Message from S3-->" + convertedObject);
+                            log.info("Message from S3-->" + convertedObject);
+                            JsonElement onBoardingJsonElement = convertedObject.get("Message");
+                            if (!onBoardingJsonElement.isJsonNull()) {
+                                String jsonString = String.valueOf(onBoardingJsonElement);
+                                jsonString = jsonString.replace("\\", "");
+                                jsonString = jsonString.substring(1, jsonString.length() - 1);
+                                JsonObject jsonObject = new Gson().fromJson(jsonString, JsonObject.class);
+                                OnboardUserDTO onboardUserDTO = new Gson().fromJson(jsonObject.toString(), OnboardUserDTO.class);
+                                onboardUserDTOList.add(onboardUserDTO);
+                            } else {
+                                log.info("No Valid Object found");
+                            }
+
                         }
                 );
             } else {
-                logger.info("No messages found");
+                log.info("No messages found");
             }
         } catch (SqsException e) {
-            logger.error("Error in reading message from SQS-->" + e.awsErrorDetails().errorMessage());
+            log.error("Error in reading message from SQS-->" + e.awsErrorDetails().errorMessage());
         }
+        return onboardUserDTOList;
     }
 }
