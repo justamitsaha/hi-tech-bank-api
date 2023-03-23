1582cf1ffdc4958fef9f8d87b10ea735d0b396bb
justamitsaha <justamitsaha@gmail.com>
2023-03-23 06:50:02 +0530

    Initiate mail sending

--- aws-service/src/main/java/com/saha/amit/controller/AwsSqsController.java
+++ aws-service/src/main/java/com/saha/amit/controller/AwsSqsController.java
@@ -1,21 +1,46 @@
 package com.saha.amit.controller;
 
+import com.saha.amit.dto.OnboardUserDTO;
+import com.saha.amit.dto.OtpMailSenderDTO;
 import com.saha.amit.service.AwsSqsUtilityService;
+import com.saha.amit.service.MailSenderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class AwsSqsController {
+    private static final Logger log = LoggerFactory.getLogger(AwsSqsController.class);
     @Autowired
     AwsSqsUtilityService awsSqsUtilityService;
-    private static final Logger log = LoggerFactory.getLogger(AwsSqsController.class);
+    @Autowired
+    MailSenderService mailSenderService;
+
     @GetMapping("public/readMessageFromQueue")
     public boolean readMessageFromQueue() {
         log.info("Inside readMessageFromQueue");
-        awsSqsUtilityService.readMessageFromQueue();
+        List<OnboardUserDTO> onboardUserDTOList = awsSqsUtilityService.readMessageFromQueue();
+        List<OtpMailSenderDTO> otpMailSenderDTOList = new ArrayList<>();
+        onboardUserDTOList.forEach(onboardUserDTO -> {
+            OtpMailSenderDTO otpMailSenderDTO = new OtpMailSenderDTO();
+            OtpMailSenderDTO otpMailSenderDTO1 = new OtpMailSenderDTO();
+            otpMailSenderDTO.setEmail(onboardUserDTO.getEmail());
+            otpMailSenderDTO.setOtp(onboardUserDTO.getEmailOtp());
+            otpMailSenderDTO1.setEmail(onboardUserDTO.getEmail());
+            otpMailSenderDTO1.setOtp(onboardUserDTO.getTextOtp());
+            otpMailSenderDTOList.add(otpMailSenderDTO);
+            otpMailSenderDTOList.add(otpMailSenderDTO1);
+        });
+
+        otpMailSenderDTOList.stream().map(otpMailSenderDTO -> {
+            return mailSenderService.sendMail(otpMailSenderDTO);
+        }).forEach(s -> {
+            log.info("Mail send status for " + otpMailSenderDTOList + " -->" + s);
+        });
         return true;
     }
 }
