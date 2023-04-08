49b7e48cd75ccc97312b2035b04d0c2f1f0c5f22
justamitsaha <justamitsaha@gmail.com>
2023-04-08 08:53:30 +0530

    Code cleanup

--- onboard-user-service/src/main/java/com/saha/amit/controller/OnboardingController.java
+++ onboard-user-service/src/main/java/com/saha/amit/controller/OnboardingController.java
@@ -14,7 +14,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
@@ -23,35 +24,33 @@ import java.util.List;
 @RefreshScope
 public class OnboardingController {
 
+    private static final Logger log = LoggerFactory.getLogger(OnboardingController.class);
+    @Autowired
+    OnboardingService onboardingService;
     @Value("${my.greeting: default lau}")
     private String config;
-
     @Value("${my.list.values}")
     private List<String> listStrings;
-
     @Value("${demo.property: noValue}")
     private String testConfig;
 
-    @Autowired
-    OnboardingService onboardingService;
-
-    private static final Logger log= LoggerFactory.getLogger(OnboardingController.class);
-
     @PostMapping(value = "/public/applyToOpenAccount", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<String> applyToOpenAccount(@RequestPart OnboardUserDTO onboardUser, @RequestPart MultipartFile multipartFile1, @RequestPart MultipartFile multipartFile2) throws IOException {
+    public ResponseEntity<String> applyToOpenAccount(@RequestPart OnboardUserDTO onboardUser,
+                                                     @RequestPart MultipartFile multipartFile1,
+                                                     @RequestPart MultipartFile multipartFile2) throws IOException {
         log.info("Start applyToOpenAccount controller");
         var response = onboardingService.applyToOpenAccount(onboardUser, multipartFile1, multipartFile2);
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(response);
     }
 
     @GetMapping(value = "/public/test")
-    public ResponseEntity<String> test(){
-        return ResponseEntity.status(HttpStatus.ACCEPTED).body(config +"<-->"+ listStrings.toString()+"<-->"+testConfig);
+    public ResponseEntity<String> test() {
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(config + "<-->" + listStrings.toString() + "<-->" + testConfig);
     }
 
     @PutMapping(value = "/public/validateOtp")
-    public ResponseEntity<Boolean> validateOTP(@RequestBody ValidateOtpDTO validateOtpDTO){
-        log.info("Start validateOTP controller for "+ validateOtpDTO.getType());
+    public ResponseEntity<Boolean> validateOTP(@RequestBody ValidateOtpDTO validateOtpDTO) {
+        log.info("Start validateOTP controller for " + validateOtpDTO.getType());
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(onboardingService.validateOtp(validateOtpDTO));
     }
 }
