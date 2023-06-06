3d564146905366fb30d8d0f8f365a2b2dabb19e7
justamitsaha <justamitsaha@gmail.com>
2023-06-06 21:56:07 +0530

    script change

--- scripts/local_run_script.bat
+++ scripts/local_run_script.bat
@@ -3,9 +3,9 @@
 cd ../config-service/
 set /p user_action="Do you want to run config service?enter y/n"
 if "%user_action%"=="y" (
-    mvn clean install
+    start mvn clean install
     cd target/
-    start cmd /c "java -jar config-service-1.0-SNAPSHOT.jar"
+    start java -jar config-service-1.0-SNAPSHOT.jar
     cd ../
 ) else (
     echo "Skipping config server"
@@ -14,9 +14,9 @@ if "%user_action%"=="y" (
 cd ../discovery-service/
 set /p user_action="Do you want to run discovery service?enter y/n"
 if "%user_action%"=="y" (
-    mvn clean install
+    start mvn clean install
     cd target/
-    start cmd /c "java -jar discovery-service-1.0-SNAPSHOT.jar >> output.txt"
+    start java -jar discovery-service-1.0-SNAPSHOT.jar >> output.txt
     cd ../
 ) else (
     echo "Skipping config server"
@@ -25,9 +25,9 @@ if "%user_action%"=="y" (
 cd ../api-gate-way-service/
 set /p user_action="Do you want to run api gateway service?enter y/n"
 if "%user_action%"=="y" (
-    mvn clean install
+    start mvn clean install
     cd target/
-    start cmd /c "java -jar api-gate-way-service-1.0-SNAPSHOT.jar"
+    start java -jar api-gate-way-service-1.0-SNAPSHOT.jar
 ) else (
     echo "Skipping config server"
 )
@@ -35,9 +35,9 @@ if "%user_action%"=="y" (
 cd ../redis-cache-service/
 set /p user_action="Do you want to run redis-cache-service?enter y/n"
 if "%user_action%"=="y" (
-    mvn clean install
+    start mvn clean install
     cd target/
-    start cmd /c "java -jar redis-cache-service-1.0-SNAPSHOT.jar"
+    start java -jar redis-cache-service-1.0-SNAPSHOT.jar
     cd ../
 ) else (
     echo "Skipping config server"
@@ -46,9 +46,9 @@ if "%user_action%"=="y" (
 cd ../onboard-user-service/
 set /p user_action="Do you want to run onboard-user-service?enter y/n"
 if "%user_action%"=="y" (
-    mvn clean install
+    start mvn clean install
     cd target/
-    start cmd /c "java -jar onboard-user-service-1.0-SNAPSHOT.jar"
+    start java -jar onboard-user-service-1.0-SNAPSHOT.jar
     cd ../
 ) else (
     echo "Skipping config server"
